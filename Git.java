03e624070afd7671aaf87662ac09bf262a9258d6
githubjenkin <103298933+githubjenkin@users.noreply.github.com>
2022-04-13 08:18:48 +0530

    Update Git.java

--- Git.java
+++ Git.java
@@ -2,5 +2,7 @@ class Git{
  public static void main(String args[]) {
    System.out.println(" *  *  *");
    System.out.println(" Git Organization ");
+   System.out.println(" Forking ");
+   System.out.println(" GitHub ");
   }
 }
