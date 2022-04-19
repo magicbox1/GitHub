1df9f0a08e825df348751b9d0cf23002699b3e22
magicbox1 <103236935+magicbox1@users.noreply.github.com>
2022-04-19 10:04:48 +0530

    Update Git.java

--- Git.java
+++ Git.java
@@ -1,6 +1,7 @@
 class Git{
  public static void main(String args[]) {
    System.out.println(" *  *  *");
-   System.out.println(" Git Organization ");
+   System.out.println(" -------- ");
+  System.out.println(" Organization");
   }
 }
